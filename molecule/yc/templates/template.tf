29941fb77185aeb76335cca9174efd70fe59e3f8
EKbfh <earthquake_90@mail.ru>
2020-03-27 01:29:33 +0300

    Add platform_id as Cascade Lake cpus

--- molecule/yc/templates/template.tf
+++ molecule/yc/templates/template.tf
@@ -35,7 +35,7 @@ resource "yandex_compute_instance" "vm-1" {
     serial-port-enable = 1  }
 }
 
-platform_id = standard-v2
+  platform_id = "standard-v2"
 
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface[0].ip_address
